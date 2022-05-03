3ea41b827c1b1aec3ae52de766f7a2654f32aceb
Ebrahim Hosseini <sehosseini@me.com>
2022-05-03 12:45:42 +0430

    add function comments

--- StackExchangeUsers/StackExchangeUsers/Manager/KFImageView.swift
+++ StackExchangeUsers/StackExchangeUsers/Manager/KFImageView.swift
@@ -54,6 +54,15 @@ class KFImageView: UIImageView {
     clipsToBounds = true
   }
 
+
+  /// Download image from kingfisher lib.
+  /// Set the default placeholder for any of images.
+  /// Set a transition to image view when image loading.
+  /// Set the progress block t download the download progress block
+  ///
+  /// - Parameters:
+  ///   - resource: an initial download url
+  ///   - completion: result of kingfisher to get an image from server
   private func setImageWithKingfisher(_ resource: ImageResource,
                                       completion: ((Result<RetrieveImageResult, KingfisherError>) -> Void)?) {
     self.kf.setImage(with: resource,
@@ -68,6 +77,8 @@ class KFImageView: UIImageView {
     print("LOAD IMAGE RESULT-> \(data)")
   }
 
+  /// Get the response from Result of response
+  /// - Parameter response: a response type which get from kingfisher downloder
   private func getResponse(_ response: Result<RetrieveImageResult, KingfisherError>) {
     switch response {
     case .success(let result):
@@ -77,6 +88,10 @@ class KFImageView: UIImageView {
     }
   }
 
+  /// Set image and get response result and show the result is success or failure
+  /// - Parameters:
+  ///   - resource: an initial download url
+  ///   - completion: result of setImage function which download image from kingfisher
   private func setImage(with resource: ImageResource,
                         completion: ((Result<RetrieveImageResult, KingfisherError>) -> Void)?) {
     setImageWithKingfisher(resource) { [weak self] response in
@@ -86,16 +101,21 @@ class KFImageView: UIImageView {
     }
   }
 
+  /// Cancel download tasks by kingfisher
   private func cancelDownloadTask() {
     self.kf.cancelDownloadTask()
   }
 
+  /// Get resource from download url
+  /// - Parameter completion: result from download url
   private func getResource(_ completion: (ImageResource) -> Void) {
     guard let url = imageUri, let downloadURL = URL(string: url) else { return }
     let resource = ImageResource(downloadURL: downloadURL)
     completion(resource)
   }
 
+  /// Cancel any download tasks and set the image to image view
+  /// - Parameter completion: result of setImage function which download image from kingfisher
   private func setImage(_ completion: ((Result<RetrieveImageResult, KingfisherError>) -> Void)? = nil) {
     cancelDownloadTask()
     getResource { resource in
